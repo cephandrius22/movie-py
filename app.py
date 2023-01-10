19816222c8d6dc8af318634f89f64eabee2c77be
Joachim Wright <joewright1610@gmail.com>
2023-01-09 20:19:47 -0500

    Add some type hinting and to_dict() method to db classes.

--- app.py
+++ app.py
@@ -28,6 +28,15 @@ class Movie(db.Model):
     title = db.Column(db.Text, nullable=True)
     budget = db.Column(db.Integer, nullable=True)
 
+    def to_dict(self):
+        return {
+            "budget": self.budget,
+            "title": self.title,
+            "release_date": self.release_date.strftime("%Y-%m-%d"),
+            "runtime": self.runtime,
+            "id": self.id,
+        }
+
 
 class Actor(db.Model):
     __tablename__ = "actor_credits"
@@ -37,6 +46,15 @@ class Actor(db.Model):
     movie_id = db.Column(db.Integer)
     name = db.Column(db.Text)
     character = db.Column(db.Text)
+
+    def to_dict(self):
+        return {
+            "character": self.character,
+            "name": self.name,
+            "id": self.id,
+            "movie_id": self.movie_id,
+            "credit_id": self.credit_id,
+        }
 
 
 class Crew(db.Model):
@@ -48,6 +66,15 @@ class Crew(db.Model):
     department = db.Column(db.Text)
     job = db.Column(db.Text)
 
+    def to_dict(self):
+        return {
+            "job": self.job,
+            "deparment": self.department,
+            "name": self.name,
+            "movie_id": self.movie_id,
+            "credit_id": self.credit_id,
+        }
+
 
 @app.route("/")
 def index():
@@ -56,24 +83,16 @@ def index():
 
 @app.route("/movies", methods=["GET"])
 def get_movie_collection():
-    movies = Movie.query.limit(50).all()
+    movies: list[Movie] = Movie.query.limit(50).all()
     movie_list = []
     for movie in movies:
-        movie_list += [
-            {
-                "budget": movie.budget,
-                "title": movie.title,
-                "release_date": movie.release_date.strftime("%Y-%m-%d"),
-                "runtime": movie.runtime,
-                "id": movie.id,
-            }
-        ]
+        movie_list += [movie.to_dict()]
     return jsonify(movie_list)
 
 
 @app.route("/movies/<int:id>", methods=["GET"])
 def get_movie_resource(id):
-    movie = Movie.query.filter(Movie.id == id)
+    movie: list[Movie] = Movie.query.filter(Movie.id == id)
     try:
         movie = movie[0]
     except IndexError:
@@ -84,37 +103,21 @@ def get_movie_resource(id):
     # This should not happen but we should probably handle it
     # regardless.
 
-    return jsonify(
-        {
-            "budget": movie.budget,
-            "title": movie.title,
-            "release_date": movie.release_date.strftime("%Y-%m-%d"),
-            "runtime": movie.runtime,
-            "id": movie.id,
-        }
-    )
+    return jsonify(movie.to_dict())
 
 
 @app.route("/actors", methods=["GET"])
 def get_actor_collection():
-    actors = Actor.query.limit(50).all()
+    actors: list[Actor] = Actor.query.limit(50).all()
     actor_list = []
     for actor in actors:
-        actor_list += [
-            {
-                "character": actor.character,
-                "name": actor.name,
-                "id": actor.id,
-                "movie_id": actor.movie_id,
-                "credit_id": actor.credit_id,
-            }
-        ]
+        actor_list += [actor.to_dict()]
     return jsonify(actor_list)
 
 
 @app.route("/actors/<int:id>", methods=["GET"])
 def get_actor_resource(id):
-    actor = Actor.query.filter(Actor.credit_id == id)
+    actor: list[Actor] = Actor.query.filter(Actor.credit_id == id)
     try:
         actor = actor[0]
     except IndexError:
@@ -125,37 +128,21 @@ def get_actor_resource(id):
     # This should not happen but we should probably handle it
     # regardless.
 
-    return jsonify(
-        {
-            "character": actor.character,
-            "name": actor.name,
-            "id": actor.id,
-            "movie_id": actor.movie_id,
-            "credit_id": actor.credit_id,
-        }
-    )
+    return jsonify(actor.to_dict())
 
 
 @app.route("/crew", methods=["GET"])
 def get_crews():
-    crew = Crew.query.limit(50).all()
+    crew: list[Crew] = Crew.query.limit(50).all()
     crew_list = []
     for c in crew:
-        crew_list += [
-            {
-                "job": c.job,
-                "deparment": c.department,
-                "name": c.name,
-                "movie_id": c.movie_id,
-                "credit_id": c.credit_id,
-            }
-        ]
+        crew_list += [c.to_dict()]
     return jsonify(crew_list)
 
 
 @app.route("/crew/<int:id>", methods=["GET"])
 def get_crew_resource(id):
-    crew = Crew.query.filter(Crew.credit_id == id)
+    crew: list[Crew] = Crew.query.filter(Crew.credit_id == id)
     try:
         crew = crew[0]
     except IndexError:
@@ -166,15 +153,7 @@ def get_crew_resource(id):
     # This should not happen but we should probably handle it
     # regardless.
 
-    return jsonify(
-        {
-            "job": crew.job,
-            "deparment": crew.department,
-            "name": crew.name,
-            "movie_id": crew.movie_id,
-            "credit_id": crew.credit_id,
-        }
-    )
+    return jsonify(crew.to_dict())
 
 
 if __name__ == "__main__":
